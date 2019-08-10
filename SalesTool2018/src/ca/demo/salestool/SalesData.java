46c59ee0b1d638ea17fb68e89e11c95ec1d9254d
desa1 <desa1@gmail.com>
2019-08-10 16:19:34 -0400

    Partial Implementation of the numbers

--- SalesTool2018/src/ca/demo/salestool/SalesData.java
+++ SalesTool2018/src/ca/demo/salestool/SalesData.java
@@ -5,7 +5,10 @@ public class SalesData {
 	private int data[] = {0, 4, 42};
 	
 	public void display() {
-		System.out.println("TODO: Implement data  display");
+
+		for(int i = 0; i < data.length; i++) {
+			System.out.println("");
+		}
 	}
 	
 }
