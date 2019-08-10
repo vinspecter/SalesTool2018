47f7e4ab31b98a73660baf008932951cfcb8d8c7
desa2 <desa2@gmail.com>
2019-08-10 18:44:39 -0400

    sum Created in the method..

--- SalesTool2018/src/ca/demo/salestool/SalesData.java
+++ SalesTool2018/src/ca/demo/salestool/SalesData.java
@@ -5,10 +5,14 @@ public class SalesData {
 	private int data[] = {0, 4, 42};
 	
 	public void display() {
+		int sum  = 0;
 		System.out.println("Data: ");
 		for(int i = 0; i < data.length; i++) {
 			System.out.println("Next value: " + data[i]);
+			sum += data[i];
 		}
+		
+		System.out.println("Sum is: " +  sum);
 	}
 	
 }
