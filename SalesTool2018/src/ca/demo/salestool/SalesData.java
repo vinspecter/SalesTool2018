92be7a7669998313b21ae99174b399b1e72cb194
dev1 <dev1@gmail.com>
2019-08-10 18:54:24 -0400

    Change default values and the for loop

--- SalesTool2018/src/ca/demo/salestool/SalesData.java
+++ SalesTool2018/src/ca/demo/salestool/SalesData.java
@@ -2,11 +2,12 @@ package ca.demo.salestool;
 
 public class SalesData {
 
-	private int data[] = {0, 4, 42};
+	private int data[] = {-1, 0, 4, 42, 84};
 	
 	public void display() {
 		System.out.println("Data: ");
 		for(int i = 0; i < data.length; i++) {
+			
 			System.out.println("Next value: " + data[i]);
 		}
 	}
