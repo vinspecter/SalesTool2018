8c0a51bf61363efde4f8ad533ecafdb75e2f1d1a
desa2 <desa2@gmail.com>
2019-08-10 19:32:58 -0400

    cambio for loop

--- SalesTool2018/src/ca/demo/salestool/SalesData.java
+++ SalesTool2018/src/ca/demo/salestool/SalesData.java
@@ -2,15 +2,15 @@ package ca.demo.salestool;
 
 public class SalesData {
 
-	private int data[] = {-1, 0, 4, 42, 84};
+	 int data[] = {-1, 0, 4, 42, 84};
 	
 	public void display() {
 		int sum  = 0;
 		System.out.println("Data: ");
-		for(int i = 0; i < data.length; i++) {
+		for(int i : data) {
 			
-			System.out.println("Next value: " + data[i]);
-			sum += data[i];
+			System.out.println("Next value: " + i);
+			sum += i;
 		}
 		
 		System.out.println("Sum is: " +  sum);
