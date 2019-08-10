9c65de78022fb8b60a14f549d3189dadf7525a34
desa1 <desa1@gmail.com>
2019-08-10 16:56:47 -0400

    completed display data in a salesData

--- SalesTool2018/src/ca/demo/salestool/SalesData.java
+++ SalesTool2018/src/ca/demo/salestool/SalesData.java
@@ -5,9 +5,9 @@ public class SalesData {
 	private int data[] = {0, 4, 42};
 	
 	public void display() {
-
+		System.out.println("Data: ");
 		for(int i = 0; i < data.length; i++) {
-			System.out.println("");
+			System.out.println("Next value: " + data[i]);
 		}
 	}
 	
